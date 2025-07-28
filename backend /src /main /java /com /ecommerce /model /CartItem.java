63e6c16543ad2ffb8ed048540ec33481f3edfca7
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:45:07 +0300

    Update CartItem.java

--- backend /src /main /java /com /ecommerce /model /CartItem.java
+++ backend /src /main /java /com /ecommerce /model /CartItem.java
@@ -1,1 +1,15 @@
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+    // Getters & Setters
+}
