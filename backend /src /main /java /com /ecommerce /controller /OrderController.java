54b34021f326942b9b0d18837dd54d735a6e7483
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:39:54 +0300

    Update OrderController.java

--- backend /src /main /java /com /ecommerce /controller /OrderController.java
+++ backend /src /main /java /com /ecommerce /controller /OrderController.java
@@ -1,1 +1,11 @@
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
 
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+}
