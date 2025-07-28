5d4da35744b8e5d374b58f18c1d8343e37596007
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:19:59 +0300

    Update User.java

--- backend /src /main /java /com /ecommerce /model /User.java
+++ backend /src /main /java /com /ecommerce /model /User.java
@@ -1,1 +1,9 @@
-
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String role; // "USER" ή "ADMIN"
+    // Getters & Setters
+}
