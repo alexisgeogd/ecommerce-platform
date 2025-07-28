fd7e528b8afcdefc1e3151edfcb6d2850e45343c
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:52:49 +0300

    Update Review.java

--- backend /src /main /java /com /ecommerce /model /Review.java
+++ backend /src /main /java /com /ecommerce /model /Review.java
@@ -1,1 +1,16 @@
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Product product;
+
+    private int rating; // 1-5
+    private String comment;
+    // Getters & Setters
+}
