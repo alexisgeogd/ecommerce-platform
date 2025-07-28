a3efc5e7b70342fd6a5130a20ff6b07349f883fa
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:38:56 +0300

    Update ProductController.java

--- backend /src /main /java /com /ecommerce /controller /ProductController.java
+++ backend /src /main /java /com /ecommerce /controller /ProductController.java
@@ -1,1 +1,16 @@
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
 
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+}
