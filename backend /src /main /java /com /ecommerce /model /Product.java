7266184ee638077d77f08f0a58de86a8d43ab6a4
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:21:08 +0300

    Update Product.java

--- backend /src /main /java /com /ecommerce /model /Product.java
+++ backend /src /main /java /com /ecommerce /model /Product.java
@@ -1,1 +1,9 @@
-
+@Entity
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private int stock;
+    // Getters & Setters
+}
