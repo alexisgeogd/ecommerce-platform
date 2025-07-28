02281da9f898dfeaaa7f240116a679131da8acf5
Alexis Georgiou <152292817+alexisgeogd@users.noreply.github.com>
2025-07-28 11:44:31 +0300

    Update Cart.java

--- backend /src /main /java /com /ecommerce /model /Cart.java
+++ backend /src /main /java /com /ecommerce /model /Cart.java
@@ -1,1 +1,15 @@
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @OneToOne
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> items = new ArrayList<>();
+
+    private double totalPrice;
+    // Getters & Setters
+}
